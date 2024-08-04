96ac26e2e42ddb6fc07420b26419a6dfa3c7695e
tctctctctc <935706071@qq.com>
2024-08-04 09:44:14 +0800

    feat: 非路由组件静态页面搭建

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,145 @@
+<template>
+  <footer>
+    <div class="footer-container">
+      <div class="footerList">
+        <div class="footerItem">
+          <h4>购物指南</h4>
+          <ul class="footerItemCon">
+            <li>购物流程</li>
+            <li>会员介绍</li>
+            <li>生活旅行/团购</li>
+            <li>常见问题</li>
+            <li>购物指南</li>
+          </ul>
+        </div>
+        <div class="footerItem">
+          <h4>配送方式</h4>
+          <ul class="footerItemCon">
+            <li>上门自提</li>
+            <li>211限时达</li>
+            <li>配送服务查询</li>
+            <li>配送费收取标准</li>
+            <li>海外配送</li>
+          </ul>
+        </div>
+        <div class="footerItem">
+          <h4>支付方式</h4>
+          <ul class="footerItemCon">
+            <li>货到付款</li>
+            <li>在线支付</li>
+            <li>分期付款</li>
+            <li>邮局汇款</li>
+            <li>公司转账</li>
+          </ul>
+        </div>
+        <div class="footerItem">
+          <h4>售后服务</h4>
+          <ul class="footerItemCon">
+            <li>售后政策</li>
+            <li>价格保护</li>
+            <li>退款说明</li>
+            <li>返修/退换货</li>
+            <li>取消订单</li>
+          </ul>
+        </div>
+        <div class="footerItem">
+          <h4>特色服务</h4>
+          <ul class="footerItemCon">
+            <li>夺宝岛</li>
+            <li>DIY装机</li>
+            <li>延保服务</li>
+            <li>尚品汇E卡</li>
+            <li>尚品汇通信</li>
+          </ul>
+        </div>
+        <div class="footerItem">
+          <h4>帮助中心</h4>
+        </div>
+      </div>
+      <div class="copyright">
+        <ul class="helpLink">
+          <li>
+            关于我们
+            <span class="space"></span>
+          </li>
+          <li>
+            联系我们
+            <span class="space"></span>
+          </li>
+          <li>
+            关于我们
+            <span class="space"></span>
+          </li>
+          <li>
+            商家入驻
+            <span class="space"></span>
+          </li>
+          <li>
+            营销中心
+            <span class="space"></span>
+          </li>
+          <li>
+            友情链接
+            <span class="space"></span>
+          </li>
+          <li>
+            关于我们
+            <span class="space"></span>
+          </li>
+          <li>
+            联系我们
+            <span class="space"></span>
+          </li>
+        </ul>
+        <p>地址：长沙市天心区暮云路街道</p>
+        <p>湘ICP备19006379号</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="less">
+footer {
+  background-color: #eaeaea;
+  padding-bottom: 30px;
+
+  .footer-container {
+    width: 1200px;
+    margin: 0 auto;
+
+    .footerList {
+      display: flex;
+      justify-content: space-around;
+      padding: 20px;
+    }
+
+    .copyright {
+      text-align: center;
+      margin-top: 50px;
+
+      .helpLink {
+        display: flex;
+        justify-content: center;
+
+        li {
+          margin-left: 10px;
+          padding-right: 10px;
+          border-right: 2px solid #b3aeae;
+
+          &:first-child {
+            margin-left: 0;
+          }
+
+          &:last-child {
+            border: none;
+          }
+        }
+      }
+
+      p {
+        margin: 10px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <header>
+    <!-- 头部第一行 -->
+    <div class="headTop">
+      <!-- 版心元素 -->
+      <div class="headTopMain">
+        <!-- 左边 -->
+        <div class="headTopLeft">
+          <h5>尚品汇欢迎您！&nbsp;请</h5>
+          <p>
+            <a href="javascript:;">登录</a>
+            <a href="javascript:;">免费注册</a>
+          </p>
+        </div>
+        <!-- 右边 -->
+        <div class="headTopRight">
+          <nav>
+            <a href="javascript:;">我的订单</a>
+            <a href="javascript:;">我的购物车</a>
+            <a href="javascript:;">我的尚品汇</a>
+            <a href="javascript:;">尚品汇会员</a>
+            <a href="javascript:;">企业采购</a>
+            <a href="javascript:;">关注尚品汇</a>
+            <a href="javascript:;">合作招商</a>
+            <a href="javascript:;">商家后台</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <!-- 头部第二行 -->
+    <div class="headBottom">
+      <!-- 版心 -->
+      <div class="headBottomMain">
+        <!-- logo -->
+        <h1>
+          <a href="javascript:;">
+            <img src="@/assets/images/logo.png">
+          </a>
+        </h1>
+        <!-- 搜索 -->
+        <div class="search">
+          <input type="text" placeholder="请输入搜索内容">
+          <input type="submit" value="搜索">
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="less">
+.headTop {
+  background-color: #eaeaea;
+  height: 30px;
+
+  .headTopMain {
+    width: 1200px;
+    margin: 0 auto;
+    line-height: 30px;
+    display: flex;
+    justify-content: space-between;
+
+    .headTopLeft {
+      display: flex;
+
+      p {
+        a {
+          margin-right: 5px;
+        }
+        a:first-child {
+          border-right: 1px solid #b3aeae;
+          padding-right: 5px;
+        }
+      }
+    }
+
+    .headTopRight {
+      nav {
+        a {
+          margin-left: 10px;
+          padding-right: 10px;
+          border-right: 1px solid #b3aeae;
+          &:last-child {
+            border: none;
+            padding-right: 0;
+          }
+        }
+      }
+    }
+  }
+}
+
+.headBottom {
+  .headBottomMain {
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+
+    h1 {
+      a {
+        width: 175px;
+        height: 56px;
+        margin: 25px 45px;
+      }
+    }
+
+    .search {
+      margin-top: 35px;
+      input {
+        height: 32px;
+        &:first-child {
+          width: 490px;
+          padding: 0 4px;
+          border: 3px solid #ea4a46;
+          box-sizing: border-box;
+        }
+        &:last-child {
+          width: 68px;
+          background: #ea4a36;
+          color: #fff;
+          cursor: pointer;
+          border: none;
+        }
+        // 修饰 placeholder
+        &::-webkit-input-placeholder {
+          color: yellowgreen;
+        }
+      }
+    }
+  }
+}
+</style>
