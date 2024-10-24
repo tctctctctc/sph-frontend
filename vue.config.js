27052e9443ccf326955ec7b8e9eb464bade5e302
tctctctctc <935706071@qq.com>
2024-10-24 23:35:58 +0800

    feat: 配置代理

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: '/' + process.env.VUE_APP_PUBLIC_PATH,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://gmall-h5-api.atguigu.cn'
+      }
+    }
+  }
 })
